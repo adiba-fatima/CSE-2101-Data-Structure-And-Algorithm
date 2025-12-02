2356142718049651f02e8743aae8bc44f5baeaa6
Adiba Fatima <adibafatima1006@gmail.com>
2025-12-03 01:17:24 +0600

    Optimize bubblesort with early exit condition
    Added optimization to bubblesort to skip unnecessary passes if the array is already sorted.

--- Lab/Chapter 4/bubblesort.cpp
+++ Lab/Chapter 4/bubblesort.cpp
@@ -1,17 +1,20 @@
-#include <iostream>
+#include <iostream>//complexity o(n^2)
 using namespace std;
 void bubblesort(int LA[],int n)
 {
     for(int i=0; i<n-1;i++)
     {
+        bool isswapped=false; //(optimising)deciding if the array is already sorted. if not then the loop will be skipped and already sorted arrays will not run the loops
         for(int j=0; j<n-i-1;j++)
         {
             if(LA[j]>LA[j+1])
             {
                 swap(LA[j],LA[j+1]);
-                
+                isswapped=true;
             }
         }
+        if(!isswapped)
+            break;
     }
 }
 void printarray(int LA[],int n)
@@ -29,4 +32,5 @@ int main()
     cout<<"Sorted array: \n";
     printarray(LA,n);
     return 0;
+
 }
