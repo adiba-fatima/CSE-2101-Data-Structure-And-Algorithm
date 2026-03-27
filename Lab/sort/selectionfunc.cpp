f1dce4b8d14cfba6dd77f6bfd85c4870dbfbb892
Adiba Fatima <adibafatima1006@gmail.com>
2026-03-27 11:39:50 +0600

    Add files via upload

---
+++ Lab/sort/selectionfunc.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+void selectionsort(int arr[],int n) //o(n^2)
+{
+    for(int i=0;i<n-1;i++)
+    {
+    int minIndex=i;
+    for(int j=i+1; j<n; j++)
+    {
+        if(arr[j]<arr[minIndex])
+       { minIndex=j;}
+    }
+    swap(arr[i],arr[minIndex]);
+}
+}
+int main()
+{
+    int arr[]={3,2,6,4,1};
+    int n=5;
+    selectionsort(arr,n);
+    for (int i=0; i<n;i++)
+    cout<<arr[i]<<" ";
+}
